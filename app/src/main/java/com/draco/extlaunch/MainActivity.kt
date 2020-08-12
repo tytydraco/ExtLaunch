16758f9fbd07d2a73245c7af6d62560356b6cda0
tytydraco <tylernij@gmail.com>
2020-08-11 17:13:25 -0700

    Initial commit
    Signed-off-by: tytydraco <tylernij@gmail.com>

---
+++ app/src/main/java/com/draco/extlaunch/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.draco.extlaunch
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
+
+class AppInfo {
+    var id = ""
+    var name = ""
+    var img: Drawable? = null
+}
+
+class MainActivity : AppCompatActivity() {
+    private fun getAppList(): ArrayList<AppInfo> {
+        val launcherIntent = Intent(Intent.ACTION_MAIN, null)
+        launcherIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        val activities = packageManager.queryIntentActivities(launcherIntent, 0)
+        val appList = ArrayList<AppInfo>()
+        for (app in activities) {
+            val id = app.activityInfo.packageName
+
+            if (id == packageName)
+                continue
+
+            val info = AppInfo()
+            info.id = id
+            info.name = app.activityInfo.loadLabel(packageManager).toString()
+            info.img = packageManager.getApplicationIcon(id)
+
+            appList.add(info)
+        }
+
+        appList.sortBy {
+            it.name.toLowerCase(Locale.getDefault())
+        }
+
+        return appList
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        recycler.setItemViewCacheSize(250)
+
+        val appInfoList = getAppList()
+        val adapter = RecyclerAdapter(appInfoList, recycler, packageManager)
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this)
+    }
+}
---
+++ app/src/main/java/com/draco/extlaunch/RecyclerAdapter.kt
@@ -0,0 +1,62 @@
+package com.draco.extlaunch
+
+import android.app.ActivityOptions
+import android.app.Service
+import android.content.pm.PackageManager
+import android.hardware.display.DisplayManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+
+class RecyclerAdapter(
+        private var appList: ArrayList<AppInfo>,
+        private val recyclerView: RecyclerView,
+        private val packageManager: PackageManager
+    ): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val img = itemView.findViewById(R.id.img) as ImageView
+        val name = itemView.findViewById(R.id.name) as TextView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return appList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val info = appList[position]
+
+        holder.img.setImageDrawable(info.img)
+        holder.img.contentDescription = info.name
+        holder.name.text = info.name
+
+        holder.itemView.setOnClickListener {
+            val intent = packageManager.getLaunchIntentForPackage(info.id)
+            val dm = recyclerView.context.getSystemService(Service.DISPLAY_SERVICE) as DisplayManager
+            val displays = dm.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION)
+
+            if (displays.isEmpty()) {
+                Toast.makeText(recyclerView.context, "No external displays detected.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            for (display in displays) {
+                try {
+                    val options = ActivityOptions.makeBasic()
+                    options.launchDisplayId = display.displayId
+                    recyclerView.context.startActivity(intent, options.toBundle())
+                } catch (_: Exception) {}
+            }
+        }
+    }
+}
